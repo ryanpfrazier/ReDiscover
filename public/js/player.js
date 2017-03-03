7161a2065f17369ffa1441f59d0913c8a891b2e7
Ryan Frazier <ryanpfrazier@Ryans-MacBook-Pro.local>
2017-03-03 10:36:42 -0700

    took out console.log

--- public/js/player.js
+++ public/js/player.js
@@ -3,8 +3,6 @@ $(document).ready(function() {
   let userID = localStorage.getItem('userID');
   let finalPlaylist = localStorage.getItem('finalPlaylist...');
 
-  console.log('finalPlaylist...', JSON.parse(finalPlaylist));
-
   let webPlayerHolder = $('#webPlayerHolder');
 
   webPlayerHolder.append(`<iframe id="webPlayer" src="https://embed.spotify.com/?uri=spotify:user:${userID}:playlist:${playlistID}&theme=white" frameborder="0" allowtransparency="true"></iframe><br><a href="custom.html" class="btn btn-default btn-lg hvr-radial-out">Make a custom playlist</a>`);
